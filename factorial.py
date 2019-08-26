81bc9176f078f2a632b3800ad1b131451160264a
janmi <627943558@qq.com>
2019-08-26 23:15:35 +0800

    refactor: update factorial

--- factorial.py
+++ factorial.py
@@ -1,3 +1,12 @@
 # 阶乘
 
-num = int(input('输入一个阶乘数字'))
+factorial = int(input('输入一个阶乘数字'))
+
+if factorial < 0:
+	print('数字必须大于0')
+elif factorial == 0:
+	print('0的阶乘是1')
+else:
+	for x in range(1, factorial):
+		factorial = factorial * x
+print(factorial)
