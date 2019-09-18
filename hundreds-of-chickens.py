41b89836a3e5e64fe5cd6042bf74383b450b5e8c
janmi <627943558@qq.com>
2019-09-18 23:04:37 +0800

    feat: add hundreds-of-chickens.py

---
+++ hundreds-of-chickens.py
@@ -0,0 +1,11 @@
+def HundredsOfChickens():
+	list = []
+	for x in range(1, 100):
+		for y in range(1, 100):
+			z = (100 - x - y)
+			if (9 * x) + (15 * y) + z == 300:
+				list.append([x,y,z])
+	
+	return list
+
+print(HundredsOfChickens())
