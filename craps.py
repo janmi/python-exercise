728d5344e2e4f1c2ec3c2160581e8740a6df0a67
janmi <627943558@qq.com>
2019-09-23 23:34:45 +0800

    feat: add tkinter

--- craps.py
+++ craps.py
@@ -7,7 +7,6 @@ def recursiveCraps(firstSum, num):
 	sum = x + y
 
 	num += 1
-	# print('sum2是' + str(sum))
 	if sum == firstSum:
 		print('第' + str(num)+ '次摇塞子点数是'+ str(sum) +'和第一次摇塞子点数'+ str(firstSum) +'一样,玩家胜')
 	elif sum == 7:
@@ -19,7 +18,6 @@ def craps(recursiveSum):
 	x = random.randint(1,6)
 	y = random.randint(1,6)
 	sum = x + y
-	# print('sum是' + str(sum))
 	if sum == 7 or sum == 11:
 		print('第一次摇塞子点数是'+ str(sum) +',玩家胜')
 	elif sum == 2 or sum == 3 or sum == 12:
---
+++ magic.py
@@ -0,0 +1,6 @@
+class User(object):
+  pass
+
+
+if __name__ == '__main__':
+  print(dir(User()))
